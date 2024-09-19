6f96fc3abd9b51d31e96508ecd5a8fbbe2a185cf
Subhakar Maurya <subhakarmauryas@gmail.com>
2024-09-19 23:14:41 +0530

    third commit

--- frontend/src/landing_page/home/LogIn.js
+++ frontend/src/landing_page/home/LogIn.js
@@ -31,7 +31,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        `${process.env.REACT_APP_API_URL}/allHoldings`,
+        `${process.env.REACT_APP_API_URL}/login`,
         {
           ...inputValue,
         },
